82419152ccd50cf791c11bcc013811bd126c7382
Tucker <tmiles7@vols.utk.edu>
2020-04-21 14:10:25 -0400

    work on hiding api key

--- GetMatrix.js
+++ GetMatrix.js
@@ -1,4 +1,15 @@
-console.log("Enter locations separated by whitespace");
+const config = require('./config.js');
 
 var locations = ["Knoxville", "Seymour", "Sevierville"];
 
+var GCPkey = config.MY_KEY;
+
+console.log(GCPkey);
+
+//Request example:
+//https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=YOUR_API_KEY
+
+//HTTP request to get distance and time to locations
+Url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=1105BridgeAvenue,Knoxville%20Tennessee&destinations=3039DawsonWay,Kodak,TN&key=" + GCPkey;
+
+// console.log(Url);
