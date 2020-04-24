51b2a90c9887a675bb16814e2036e907b6c8d1f7
Tucker <tmiles7@vols.utk.edu>
2020-04-24 11:14:29 -0400

    using jquery serializeArray to parse form

--- GetMatrix.js
+++ GetMatrix.js
@@ -1,17 +1,34 @@
 // const config = require('./config.json');
 
-var GCPkey = MY_KEY;
+$(document).ready(function() {
+    $( "form" ).on( "submit", function() {
+        console.log("Form submitted!");
 
-console.log("Enter Source");
-// source =
+        var result = {};
+        $.each($('form').serializeArray(), function() {
+            result[this.name] = this.value;
+        });
 
-console.log("Enter destinations in format: \"dest1|dest2|dest3|...\"")
-// destinations = 
+        var source = result.source;
+        var destinations = result.destinations;
 
-//source and destination 
-source = "Knoxville,TN";
-destinations = "Charlotte,NC|Sevierville,TN|Nashville, TN";
+        console.log(source);
+        console.log(destinations);
+      });
+});
 
-// Url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + sources + "&destinations=" + destinations + "&key=" + GCPkey;
+// var GCPkey = MY_KEY;
 
-// console.log(Url);
+// console.log("Enter Source");
+// // source =
+
+// console.log("Enter destinations in format: \"dest1|dest2|dest3|...\"")
+// // destinations = 
+
+// //source and destination 
+// source = "Knoxville,TN";
+// destinations = "Charlotte,NC|Sevierville,TN|Nashville, TN";
+
+// // Url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + sources + "&destinations=" + destinations + "&key=" + GCPkey;
+
+// // console.log(Url);
