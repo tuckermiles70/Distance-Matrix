851bfe38083e84c0f1e44b1954046efd2bb345c3
Tucker <tmiles7@vols.utk.edu>
2020-04-24 11:18:03 -0400

    generating correct request url on form submit

--- GetMatrix.js
+++ GetMatrix.js
@@ -1,5 +1,3 @@
-// const config = require('./config.json');
-
 $(document).ready(function() {
     $( "form" ).on( "submit", function() {
         console.log("Form submitted!");
@@ -14,21 +12,11 @@ $(document).ready(function() {
 
         console.log(source);
         console.log(destinations);
+
+        var GCPkey = MY_KEY;
+
+        Url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + source + "&destinations=" + destinations + "&key=" + GCPkey;
+        console.log(Url);
+
       });
 });
-
-// var GCPkey = MY_KEY;
-
-// console.log("Enter Source");
-// // source =
-
-// console.log("Enter destinations in format: \"dest1|dest2|dest3|...\"")
-// // destinations = 
-
-// //source and destination 
-// source = "Knoxville,TN";
-// destinations = "Charlotte,NC|Sevierville,TN|Nashville, TN";
-
-// // Url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + sources + "&destinations=" + destinations + "&key=" + GCPkey;
-
-// // console.log(Url);
