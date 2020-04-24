85165d7ceed73721739456f86f172e31256886ec
Tucker <tmiles7@vols.utk.edu>
2020-04-24 09:50:32 -0400

    added basic html form

--- GetMatrix.js
+++ GetMatrix.js
@@ -1,18 +1,17 @@
 const config = require('./config.json');
 
-var locations = ["Knoxville", "Seymour", "Sevierville"];
-
 var GCPkey = config.MY_KEY;
 
-// console.log(GCPkey);
+console.log("Enter Source");
+// source =
 
-//Request example:
-//https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=YOUR_API_KEY
+console.log("Enter destinations in format: \"dest1|dest2|dest3|...\"")
+// destinations = 
 
-//HTTP request to get distance and time to locations
-sources = ["Knoxville,TN"];
-//different destinations must be separated by a pipe
-destinations = ["Charlotte,NC|Sevierville,TN|Nashville, TN"];
+//source and destination 
+source = "Knoxville,TN";
+destinations = "Charlotte,NC|Sevierville,TN|Nashville, TN";
+
 Url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + sources + "&destinations=" + destinations + "&key=" + GCPkey;
 
 console.log(Url);
