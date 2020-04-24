efacc9970e5bd40404004d1759ea6eb301c9f44f
Tucker <tmiles7@vols.utk.edu>
2020-04-24 09:31:45 -0400

    URL format done

--- GetMatrix.js
+++ GetMatrix.js
@@ -4,14 +4,15 @@ var locations = ["Knoxville", "Seymour", "Sevierville"];
 
 var GCPkey = config.MY_KEY;
 
-console.log(GCPkey);
+// console.log(GCPkey);
 
 //Request example:
 //https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=YOUR_API_KEY
 
 //HTTP request to get distance and time to locations
-sources = ["Knoxville,TN", "Charlotte,NC"];
-destinations = ["Charlotte,NC", "Knoxville,TN"];
+sources = ["Knoxville,TN"];
+//different destinations must be separated by a pipe
+destinations = ["Charlotte,NC|Sevierville,TN|Nashville, TN"];
 Url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + sources + "&destinations=" + destinations + "&key=" + GCPkey;
 
 console.log(Url);
