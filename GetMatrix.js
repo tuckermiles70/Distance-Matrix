c1433fe7975fed63a42461154594ebf27a0138a8
Tucker <tmiles7@vols.utk.edu>
2020-04-26 12:52:25 -0400

    map and distance matrix successfully added

--- GetMatrix.js
+++ GetMatrix.js
@@ -14,29 +14,5 @@ $(document).ready(function() {
         console.log(destinations);
 
         var GCPkey = MY_KEY;
-
-        var proxy = "https://cors-anywhere.herokuapp.com/";
-
-        Url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + source + "&destinations=" + destinations + "&key=" + GCPkey;
-
-        const Http = new XMLHttpRequest();
-        Http.open("GET", Url);
-        Http.send();
-
-        Http.onreadystatechange=(e)=>{
-            console.log(Http.responseText.destination_addresses[0]);
-        }
-
-
-        // var distance;
-        // $.getJSON(Url, function(data){
-        //     // var distance = data.rows[0].elements[0].distance.text;
-        //     // alert(data.destination_addresses);
-        //     alert("success");
-        // })
-
-        // alert(distance);
-
-        alert(Url);
       });
 });
--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Plan to use GCP's [Distance Matrix API](https://developers.google.com/maps/documentation/distance-matrix/start)
 
 Ideally, when complete this will allow people who may be less tech savvy to input a list of places they go to (such as doctors offices, family/friend's houses, grocery stores, etc) and generate a nice way to view the distance and average time to get to all of them. Potentially show travel times for different days of week and times.
+
+
+https://developers.google.com/maps/documentation/javascript/reference/distance-matrix
+
+https://developers.google.com/maps/documentation/javascript/examples/distance-matrix
