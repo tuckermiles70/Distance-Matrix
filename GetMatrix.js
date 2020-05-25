5e179eae7af6e2e649c82b840f128b1d53fde45c
Tucker <tmiles7@vols.utk.edu>
2020-05-25 00:24:41 -0400

    refactor

--- GetMatrix.js
+++ GetMatrix.js
@@ -7,6 +7,26 @@ $(document).ready(function() {
             result[this.name] = this.value;
         });
 
+        const service = new google.maps.DistanceMatrixService(); // instantiate Distance Matrix service
+        const matrixOptions = {
+          origins: ["Sevierville, TN"],
+          destinations: ["Knoxville, TN"],
+          travelMode: 'DRIVING',
+          unitSystem: google.maps.UnitSystem.IMPERIAL
+        };
+      
+        // Call Distance Matrix service
+        service.getDistanceMatrix(matrixOptions, callback);
+      
+        // Callback function used to process Distance Matrix response
+        function callback(response, status) {
+          if (status !== "OK") {
+            alert("Error with distance matrix");
+            return;
+          }
+          console.log(response);        
+        }
+
         // initMap();
     });
 });
---
+++ main.js
@@ -0,0 +1,55 @@
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: {lat: 35.9606, lng: -83.9207}
+  });
+
+  const cities = [
+    {lat: 41.88, lng: -87.62}, // Chicago
+    {lat: 43.05, lng: -87.95}, // Milwaukee
+    {lat: 42.33, lng: -83.04}, // Detroit
+    {lat: 39.76, lng: -86.15}, // Indianapolis
+    {lat: 38.62, lng: -90.19} // St. Louis
+  ];
+
+  // Loop through cities, adding markers
+  for (let i=0; i<cities.length; i++) {
+    let position = cities[i]; // location of one city
+    // create marker for a city
+    let mk = new google.maps.Marker({position: position, map: map});
+  }
+
+  var result = {};
+  // $.each($('form').serializeArray(), function() {
+  //     result[this.name] = this.value;
+  // });
+
+  var source = document.getElementById('source').value; //result.source.toString();
+  // var destinations = result.destinations;
+
+  alert(source);
+
+  const service = new google.maps.DistanceMatrixService(); // instantiate Distance Matrix service
+  const matrixOptions = {
+    origins: ["Sevierville, TN"],
+    destinations: ["Knoxville, TN"],
+    travelMode: 'DRIVING',
+    unitSystem: google.maps.UnitSystem.IMPERIAL
+  };
+
+  // Call Distance Matrix service
+  service.getDistanceMatrix(matrixOptions, callback);
+
+  // Callback function used to process Distance Matrix response
+  function callback(response, status) {
+    if (status !== "OK") {
+      alert("Error with distance matrix");
+      return;
+    }
+    console.log(response);        
+  }
+  // Add Distance Matrix here
+}
+
+// export { initMap };
