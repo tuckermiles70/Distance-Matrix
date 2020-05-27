1101c1d15e6b1da85835e471a330a316c6455e1c
Tucker <tmiles7@vols.utk.edu>
2020-05-26 22:23:02 -0400

    more fiddling

--- main.js
+++ main.js
@@ -1,39 +1,48 @@
+var _origins = [];
+var _destinations = [];
+
+// $(document).ready(function() {
+//   $( "form" ).on( "submit", function() {
+//     _origins[0] = document.getElementById("source").value;
+//     _destinations[0] = document.getElementById("destination0").value;
+//     console.log("here");
+//     initMap();
+//   });
+// });
+
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: {lat: 35.9606, lng: -83.9207}
-  });
 
-  const cities = [
-    {lat: 41.88, lng: -87.62}, // Chicago
-    {lat: 43.05, lng: -87.95}, // Milwaukee
-    {lat: 42.33, lng: -83.04}, // Detroit
-    {lat: 39.76, lng: -86.15}, // Indianapolis
-    {lat: 38.62, lng: -90.19} // St. Louis
-  ];
-
-  // Loop through cities, adding markers
-  for (let i=0; i<cities.length; i++) {
-    let position = cities[i]; // location of one city
-    // create marker for a city
-    let mk = new google.maps.Marker({position: position, map: map});
-  }
-
-  var result = {};
-  // $.each($('form').serializeArray(), function() {
-  //     result[this.name] = this.value;
+  // map = new google.maps.Map(document.getElementById('map'), {
+  //   zoom: 15,
+  //   center: {lat: 35.9606, lng: -83.9207}
   // });
 
-  var source = document.getElementById('source').value; //result.source.toString();
-  // var destinations = result.destinations;
+  // const cities = [
+  //   {lat: 41.88, lng: -87.62}, // Chicago
+  //   {lat: 43.05, lng: -87.95}, // Milwaukee
+  //   {lat: 42.33, lng: -83.04}, // Detroit
+  //   {lat: 39.76, lng: -86.15}, // Indianapolis
+  //   {lat: 38.62, lng: -90.19} // St. Louis
+  // ];
 
-  alert(source);
+  // // Loop through cities, adding markers
+  // for (let i=0; i<cities.length; i++) {
+  //   let position = cities[i]; // location of one city
+  //   // create marker for a city
+  //   let mk = new google.maps.Marker({position: position, map: map});
+  // }
+
+  // _origins[0] = document.getElementById("source").value;
+  // _destinations[0] = document.getElementById("destination0").value;
+
+  _origins[0] = "sevierville tn";
+  _destinations[0] = "knoxville tn";
 
   const service = new google.maps.DistanceMatrixService(); // instantiate Distance Matrix service
   const matrixOptions = {
-    origins: ["Sevierville, TN"],
-    destinations: ["Knoxville, TN"],
+    origins: _origins,
+    destinations: _destinations,
     travelMode: 'DRIVING',
     unitSystem: google.maps.UnitSystem.IMPERIAL
   };
@@ -47,10 +56,19 @@ function initMap() {
       alert("Error with distance matrix");
       return;
     }
-    console.log(response);        
+    // console.log(response);
+    alert(response.rows[0].elements[0].distance.text);     
   }
-  // Add Distance Matrix here
 }
+
+
+$(document).ready(function() {
+  $( "form" ).on( "submit", function() {
+    initMap();
+    console.log("after initMap");
+  });
+});
+
 
 //this is what doesn't work
 // export { initMap };
